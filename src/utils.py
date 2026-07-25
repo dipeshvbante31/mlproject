c991af9f318b96a683dbc07838bf14c4759521c1
dipeshvbante31 <dipeshbante611@gmail.com>
2026-07-25 23:18:08 +0530

    updated utils.py and added hyperparameter tunning

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import numpy as np
 import dill
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path,obj):
     try:
@@ -17,12 +18,6 @@ def save_object(file_path,obj):
             
     except Exception as e:
         raise CustomException(e,sys)
-
-
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import r2_score
-from src.exception import CustomException
-import sys
 
 
 def evaluate_models(x_train, y_train, x_test, y_test, models, param):
